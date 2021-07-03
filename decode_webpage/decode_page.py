18a90d6ef5bfb2f0deb70b2f7e926d4fbf8186f9
galaxine <oezdemir50@gmail.com>
2021-07-03 09:31:20 +0200

    Finally have gotten quite a few articles, but the rest of the articles haven't been found. Maybe I should try this later with Selenium

--- decode_webpage/decode_page.py
+++ decode_webpage/decode_page.py
@@ -1,9 +1,21 @@
+# TODO: this doesn't work well enough, check the classes later.
+
 import requests
 from bs4 import BeautifulSoup
 
+
 request = requests.get("http://www.nytimes.com/")
 soup = BeautifulSoup(request.content, 'html.parser')
-list_of_span = soup.find_all('span')
-print(list_of_span)
+body = soup.body
+print(body.prettify())
+list_of_span = soup.find_all(class_=["css-xxaj7r e1lsht870", "css-3gcxf0 e1lsht870", "css-wso0n8 e1lsht870", "css-v6dkq5 e1lsht870", "css-1ylc550 e1lsht870"])
+real_list = []
 for text in list_of_span:
-    print(text.text)
+    if text.text != "":
+        real_list.append(text.text)
+print(real_list)
+
+# search in body output:
+# f = open('practicePython/decode_webpage/debug.html', 'w')
+# f.writelines(str(body))
+# commented it out because I got from it what I wanted and didn't want to format the content every time I run this
